7a6caac830ec128bd792086ab7f618a64ceea3ea
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-12-16 17:10:05 +0000

    Split out vendor chunk, optimise production builds

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,6 +5,28 @@ module.exports = (PATHS) => {
     output: {
       path: PATHS.build,
       filename  : '[name].[chunkhash:6].js'
-    }
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: module => /node_modules/.test(module.resource)
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+          name: 'manifest'
+      }),
+      // removes a lot of debugging code in React
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      // keeps hashes consistent between compilations
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      // minifies your code
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false
+        }
+      })
+    ]}
   }
-}
